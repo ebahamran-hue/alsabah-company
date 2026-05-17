e3255f5ecd6eff334ef54626b35bcd73aa23a632
Baham <ebahamran@gmail.com>
2026-05-17 03:12:39 +0300

    terms updated

--- next.config.mjs
+++ next.config.mjs
@@ -1,3 +1,15 @@
-const nextConfig = {};
+/** @type {import('next').NextConfig} */
+
+const nextConfig = {
+  async redirects() {
+    return [
+      {
+        source: "/",
+        destination: "/ar/terms",
+        permanent: false,
+      },
+    ];
+  },
+};
 
 export default nextConfig;
